e6b6d8ca2f6c088fe2f15ae7a78ac12e464c627c
Emanuel <ema.zarate90@gmail.com>
2017-08-25 20:28:15 -0300

    Componente Barra Menu

--- app/front/tp1/src/app/app.module.ts
+++ app/front/tp1/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BarraMenuComponent } from './barra-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraMenuComponent
   ],
   imports: [
     BrowserModule
---
+++ app/front/tp1/src/app/barra-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'barra-menu',
+  templateUrl: './barra-menu.component.html',
+  styleUrls: ['./barra-menu.component.css']
+})
+export class BarraMenuComponent {
+  
+}
