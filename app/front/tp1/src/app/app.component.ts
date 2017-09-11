ea73294602b5717625936d06f93c49027cb86607
Lautaro Bipe <lauti.clc@gmail.com>
2017-09-11 20:54:26 -0300

    Reverting index.html like init and request to api example added

--- app/front/tp1/src/app/app.component.ts
+++ app/front/tp1/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Moviedb 2';
+
+// A way to make requests to Movie db API
+//  constructor(private http:Http) {
+//
+//  this.http.get('https://jsonplaceholder.typicode.com/photos')
+//    .map(response => response.json())
+//    .subscribe(res => this.myData = res);
+//
+//  }
 }
