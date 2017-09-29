31e311ef2ebd1defc5f219176df0df9986030484
Emanuel <ema.zarate90@gmail.com>
2017-09-29 15:26:52 -0300

    Listado con imagenes

--- app/front/tp1/src/app/movie/pelicula.service.ts
+++ app/front/tp1/src/app/movie/pelicula.service.ts
@@ -10,8 +10,13 @@ import {Pelicula} from './pelicula';
   constructor(private http:Http){}
 
   private movieNowPlayingUrl:string = "https://api.themoviedb.org/3/movie/now_playing?api_key=f2d25e38d020b6e75e8fede7638fd12e&language=en-US&page=1";
-
+  private oneMovie:string="https://api.themoviedb.org/3/movie/";
  getMovies(): Observable <any>{
   return this.http.get(this.movieNowPlayingUrl).map((res:Response) => res.json());
+  }
+
+  getMovie(id): Observable <any>{
+    let Onemovie: string=this.oneMovie+id+"?api_key=f2d25e38d020b6e75e8fede7638fd12e&language=en-US";
+    return this.http.get(Onemovie).map((res:Response) => res.json());
   }
  }
