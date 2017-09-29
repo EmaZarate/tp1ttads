73fe4f01f8b66d47f4ad6edf93fb4061243a57a1
Emanuel <ema.zarate90@gmail.com>
2017-09-29 15:36:16 -0300

    listado con imagenes

--- app/front/tp1/src/app/movie-list/list-pelicula.component.ts
+++ app/front/tp1/src/app/movie-list/list-pelicula.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Pelicula} from '../movie/pelicula';
 import {PeliculaService} from '../movie/pelicula.service';
-<<<<<<< HEAD
+
 import { Observable }        from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
-=======
->>>>>>> d90c1fcf49fc0c33ed13ef70791c9ccda402506e
+
 
 @Component({
   selector: 'list-pelicula',
@@ -13,7 +12,7 @@ import { Subscription } from 'rxjs/Subscription';
   styleUrls: ['./list-pelicula.component.css']
 })
 export class ListPelicula  implements OnInit{
-<<<<<<< HEAD
+
   peliculas: any[];
 
   subscription: Subscription;
@@ -22,26 +21,6 @@ export class ListPelicula  implements OnInit{
   }
   ngOnInit(): void {
     this.peliculaService.getMovies().subscribe(results => this.peliculas = results);
-=======
-  peliculas: Pelicula[]=[];
-  constructor(private peliculaService:PeliculaService){
 
-    /*his.peliculas[0].title= "Minions";
-    this.peliculas[0].id=211672;
-    this.peliculas[0].poster_path="https://image.tmdb.org/t/p/w300_and_h450_bestv2/q0R4crx2SehcEEQEkYObktdeFy.jpg";
-    this.peliculas[1].title= "Guardians of the Galaxy";
-    this.peliculas[1].id=118340;
-    this.peliculas[1].poster_path="https://image.tmdb.org/t/p/w300_and_h450_bestv2/y31QB9kn3XSudA15tV7UWQ9XLuW.jpg";
-    this.peliculas[2].title= "Interstellar";
-    this.peliculas[2].id=157336;
-    this.peliculas[2].poster_path="https://image.tmdb.org/t/p/w300_and_h450_bestv2/xu9zaAevzQ5nnrsXN6JcahLnG4i.jpg";
-    this.peliculas[3].title= "Jurassic World";
-    this.peliculas[3].id=135397;
-    this.peliculas[3].poster_path="https://image.tmdb.org/t/p/w300_and_h450_bestv2/jjBgi2r5cRt36xF6iNUEhzscEcb.jpg";
-    */
-  }
-  ngOnInit(): void {
-    this.peliculaService.getMovies().then(results => this.peliculas = results);
->>>>>>> d90c1fcf49fc0c33ed13ef70791c9ccda402506e
   }
 }
--- app/front/tp1/src/app/movie/pelicula.ts
+++ app/front/tp1/src/app/movie/pelicula.ts
@@ -1,9 +1,3 @@
 export class Pelicula {
-<<<<<<< HEAD
 
-=======
-  id:number;
-  title:string;
-  poster_path:string;
->>>>>>> d90c1fcf49fc0c33ed13ef70791c9ccda402506e
 }
